2c3ca619b31ef21d9dd7dcb923e16da01ee4b995
Agrim9 <agrim9gupta@gmail.com>
2017-11-05 21:46:19 +0530

    Inbuilt init

--- mfcc_check.py
+++ mfcc_check.py
@@ -10,19 +10,29 @@ from scipy.ndimage.interpolation import shift
 from scipy.linalg import toeplitz,inv
 from scipy.fftpack import fft,fftshift,ifft,dct
 
-digit=6
-i=34
-filename="./Extracted_Feats/"+str(digit)+"/"+str(i)+".npy"
-zr=np.load(filename)
-exc_samp=[i for i in range(64) if i not in [31,32,33,34]]
-for d in range(10):
-    dist=0
-    count=1
-    for i in range(64):
-        filename="./Extracted_Feats/"+str(d)+"/"+str(i+1)+".npy"
-        tarr=np.load(filename)
-        for j in range(zr.shape[0]):
-            for k in range(tarr.shape[0]):
-                dist=dist+np.sum(np.abs(zr[j]-tarr[k]))
-                count=count+1
-    print (dist/count)
+accuracy=0
+for speak in range(1,65):
+    for digit in range(10):
+        print("Analysis for "+str(digit))
+        filename="./Extracted_Feats/"+str(digit)+"/"+str(speak)+".npy"
+        zr=np.load(filename)
+        forbidden=[int(speak/4)*4+1,int(speak/4)*4+2,int(speak/4)*4+3,int(speak/4)*4+4]
+        exc_samp=[i for i in range(64) if i not in forbidden]
+        pred=np.zeros(10)
+        for d in range(10):
+            dist=0
+            count=1
+            for i in range(64):
+                filename="./Extracted_Feats/"+str(d)+"/"+str(i+1)+".npy"
+                tarr=np.load(filename)
+                for j in range(zr.shape[0]):
+                    for k in range(tarr.shape[0]):
+                        dist=dist+np.sum(np.abs(zr[j]-tarr[k]))
+                        count=count+1
+            pred[d]=(dist/count)
+            #print(str(d)+":"+str(pred[d]))
+        predic=np.argmin(pred)
+        if(predic==digit):
+            accuracy=accuracy+1
+        print("Predicted: "+ str(predic))
+print(accuracy/640)
